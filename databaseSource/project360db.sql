36a43543b51e6ff08b5fd626c17ac645b231d101
hamza alawi <hamza_hm@hotmail.be>
2020-06-04 21:00:53 +0100

    adding subscribe function Access via ------------------------------------------------------- POST api/subscribe + updating account table + updating database

--- databaseSource/project360db.sql
+++ databaseSource/project360db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jun 04, 2020 at 06:47 PM
+-- Generation Time: Jun 04, 2020 at 08:00 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -32,21 +32,30 @@ DROP TABLE IF EXISTS `accounts`;
 CREATE TABLE IF NOT EXISTS `accounts` (
   `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `firstName` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastName` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `birthday` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `city` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `photo` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `role` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `active` tinyint(1) NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `role` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '2',
+  `active` tinyint(1) NOT NULL DEFAULT 1,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `accounts`
 --
 
-INSERT INTO `accounts` (`id`, `username`, `password`, `token`, `role`, `active`, `created_at`, `updated_at`) VALUES
-(1, 'hamza', '12345', 'RnZZc1JKeVB1eGUxdlhXdmZyMUp5QlNFakRMaGFqck9ENFFzVVNxaA==', '1', 1, NULL, '2020-06-04 18:46:35');
+INSERT INTO `accounts` (`id`, `username`, `email`, `firstName`, `lastName`, `birthday`, `phone`, `address`, `city`, `photo`, `password`, `token`, `role`, `active`, `created_at`, `updated_at`) VALUES
+(1, 'hamza', 'hamza@hotmail.com', 'hamza', 'alaoui', '01-09-1998', '', 'fes, morocco', 'fes', '', '12345', 'MVg1dFJHUjRPNFZWNlFTWHB5T2drcENvWjNXZllMNld3SHRhVDJuSQ==', '1', 1, NULL, '2020-06-04 19:51:12'),
+(2, 'alae', 'alae@hotmail.com', 'alae', 'ennouinou', '01-01-2001', NULL, 'je ss pas', 'Kenitra', NULL, '12345', 'RlBLdjFGN3g5cnQyeDE0MGV0SUdBbXF5OU1mWktkcTBDVzNWZnVyTQ==', '2', 1, '2020-06-04 19:58:26', '2020-06-04 19:58:26');
 
 -- --------------------------------------------------------
 
