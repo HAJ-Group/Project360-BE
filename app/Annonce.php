57827f739805db02dd5ded11a9a0ee02cba3f78c
Rhita Es-safi <rhitaess@gmail.com>
2020-06-06 13:41:28 +0200

    editing in some AnnonceController

--- app/Annonce.php
+++ app/Annonce.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Annonce extends Model
 {
-    //
+    protected $fillable = ['title', 'type', 'description', 'price', 'address', 'city', 'position_map', 'status', 'rent'];
+
+    protected $hidden = ['created_at', 'updated_at', 'annoncer_id'];
+
+    //Relationships
+
+    public function annoncer()
+    {
+        return $this->belongsTo('App\Annoncer');
+    }
+
 }
