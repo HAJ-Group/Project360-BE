176a7aa121b27ebe2283e4d7e44884bd2787372d
Yahya BERBECHE <yberbeche@gmail.com>
2020-06-08 12:24:01 +0100

    send email

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Contact;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use App\Mail\ContactEmail;
+use Illuminate\Support\Facades\Mail;
+
+class ContactController extends Controller {
+
+    function contact(Request $request) {
+        
+        $this->validate($request, [
+            'subject' => 'required|min:5',
+            'comment' => 'required|min:10|max:70',
+        ]);
+
+        $_SESSION['subject'] = $request->subject;
+        $_SESSION['comment'] = $request->comment;
+       // Mail::to(env('MAIL_USERNAME'))->send(new ContactEmail());
+       Mail::to('yberbeche@gmail.com')->send(new ContactEmail());
+        return response()->json('Message Sended');
+        
+
+
+    }
+
+    
+
+}
---
+++ resources/views/mailTemplate1.php
@@ -0,0 +1,11 @@
+<?php
+    $subject = $_SESSION['subject'];
+    $comment = $_SESSION['comment'];
+?>
+
+<html>
+    <body>
+        <h1><?=$subject?></h1>
+        <p><?=$comment?></p>
+    </body>
+</html>
