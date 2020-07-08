1af4c88a032edf2c67a39151f2ef2ebdbc3185fd
jaouad <jaouadaitassou@gmail.com>
2020-07-08 01:32:24 +0100

    Create the images table

---
+++ database/migrations/2020_07_08_002349_create_images_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_unicode_ci';
+            $table->bigIncrements('id');
+            $table->string('path');
+            $table->string('type');
+            $table->unsignedBigInteger('announce_id')->nullable();
+            $table->foreign('announce_id')->references('id')->on('annonces');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
