78c82ed341d876cd2c33528ae7fc64c15388056c
hamza alawi <hamza_hm@hotmail.be>
2020-07-26 00:33:57 +0100

    fix search 2.0

--- app/Http/Controllers/AnnonceController.php
+++ app/Http/Controllers/AnnonceController.php
@@ -144,9 +144,8 @@ class AnnonceController extends Controller
     public function getAnnoncesByFilters(Request $request)
     {
         $annonces = Annonce::where(function ($query) use ($request) {
-                if($request->has('keyword') and count($query->whereNotNull('title')->get())) {
+                if($request->has('keyword')) {
                     $query->where('title', 'like', '%' . $request->keyword . '%');
-                    $query->orWhere('description', 'like', '%' . $request->keyword . '%');
                 }
                 if($request->has('status') and count($query->whereNotNull('status')->get())) {
                     $query->where('status', 'like', '%' . $request->status . '%');
@@ -157,18 +156,16 @@ class AnnonceController extends Controller
                 if($request->has('city') and count($query->whereNotNull('city')->get())) {
                     $query->where('city', 'like', '%' . $request->city . '%');
                 }
+                if($request->has('surface') and count($query->whereNotNull('surface')->get())) {
+                    $query->where('surface', 'like', '%'.$request->surface.'%');
+                }
                 if($request->has('budget_min') and $request->has('budget_max') and count($query->whereNotNull('price')->get())) {
                     $query->whereBetween('price', [$request->budget_min, $request->budget_max]);
                 }
                 if($request->has('pieces') and count($query->whereNotNull('pieces')->get())) {
                     $query->where('pieces', '<=', $request->pieces);
                 }
-                if($request->has('surface') and count($query->whereNotNull('surface')->get())) {
-                    $query->where('surface', '<=', $request->surface);
-                }
             })->latest()->get();
-        $all = Annonce::all();
-        if(!count($annonces)) $annonces = $all;
         /*$annonces = Annonce::where('status', 'like', '%' . $request->status . '%')
             ->where('city', 'like', '%' . $request->city . '%')
             ->where('type', 'like', '%' . $request->type . '%')
@@ -180,7 +177,6 @@ class AnnonceController extends Controller
                     ->orWhere('description', 'like', '%' . $request->keyword . '%');
             })
             ->latest()->get();*/
-
         return response()->json(['status' => 'success', 'data' => $annonces, 200]);
     }
 
