77e57856a153ab4c65c96d70558d5390aad635eb
hamza alawi <hamza_hm@hotmail.be>
2020-09-17 03:46:35 +0100

    App 360 BACK END Laravel/Lumen VERSION READY ADD-ONS => Improve CORS Policy System => Adding 360 Images DATABASE MIGRATION SERVICES => Adding Favorites DATABASE MIGRATION SERVICES => ... author @hmz

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -25,12 +25,10 @@ class CorsMiddleware
             'Access-Control-Max-Age'           => '86400',
             'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
         ];
-
         if ($request->isMethod('OPTIONS'))
         {
             return response()->json('{"method":"OPTIONS"}', 200, $headers);
         }
-
         $response = $next($request);
         foreach($headers as $key => $value)
         {
